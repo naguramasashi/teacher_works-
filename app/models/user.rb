9a7d619448a4c113d00a90679d5583c5ed1ec2ff
EC2 Default User <ec2-user@ip-172-31-21-126.us-east-2.compute.internal>
2020-02-08 04:46:58 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: {maximum: 15}
+  
+  validates :email, presence: true
+  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  
+  validates :password, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
+  has_secure_password
+end
