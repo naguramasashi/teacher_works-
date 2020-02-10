67e809617ed7cfcd5ab59e97eb7a7d4811b663fc
EC2 Default User <ec2-user@ip-172-31-21-126.us-east-2.compute.internal>
2020-02-10 10:36:11 +0000

    　ログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   
   def create 
-    @user=User.new(name: params[:user][:name],email: params[:user][:email],password: params[:user][:password])
+    @user=User.new(name: params[:user][:name],email: params[:user][:email],subject: params[:user][:subject],password: params[:user][:password])
     if @user.save
       redirect_to root_path, success: '登録が完了しました'
     else
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   
   private
   def user_params
-  params.require(:user).permit(:name, :email, :password)
+  params.require(:user).permit(:name, :email, :subject, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: {maximum: 15}
-  
   validates :email, presence: true
   validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  
+  validates :subject, presence: true
   validates :password, presence: true
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
   has_secure_password
