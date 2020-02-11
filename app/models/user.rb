569cb277410aed3c3a5e6841a81c13d7ef162ac6
EC2 Default User <ec2-user@ip-172-31-21-126.us-east-2.compute.internal>
2020-02-11 16:14:51 +0000

    エラーメッセージを日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: {maximum: 15}
-  validates :email, presence: {message:"メールアドレスを入力してください"}
+  validates :email, presence: true
   validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   validates :subject, presence: true
-  validates :password, presence: {message:"パスワードを入力してください"}
+  validates :password, presence: true
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
   has_secure_password
   has_many :topics
