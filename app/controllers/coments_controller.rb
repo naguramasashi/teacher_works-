e10e62b31891e9e334cda10c46382fecc8fc92c4
EC2 Default User <ec2-user@ip-172-31-21-126.us-east-2.compute.internal>
2020-02-21 17:02:57 +0000

    コメント削除機能追加

--- app/controllers/coments_controller.rb
+++ app/controllers/coments_controller.rb
@@ -4,7 +4,13 @@ class ComentsController < ApplicationController
         @coment = @topic.coments.create(coment_params)
        redirect_to topic_path(@topic)
     end
+    def destroy
+        coment=Coment.find_by(id: params[:id],topic_id: params[:topic_id],)
+        coment.destroy
+        redirect_to topics_path, success: "コメント削除しました"
+    end
     def coment_params
         params.require(:coment).permit(:content,:topic_id,).merge(user_id: current_user.id)
     end
+    
 end
